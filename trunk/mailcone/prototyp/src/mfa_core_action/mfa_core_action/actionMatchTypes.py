c5ea05bf67f2565f902e33754adae131408dfecf
marco.lempen@gmail.com <marco.lempen@gmail.com@2114add4-67ca-9ca6-10be-5e31bf341269>
2011-03-29 12:23:18 +0000

    integrate action handling in rule type apply methods

--- trunk/mailcone/prototyp/src/mfa_core_action/mfa_core_action/actionMatchTypes.py
+++ trunk/mailcone/prototyp/src/mfa_core_action/mfa_core_action/actionMatchTypes.py
@@ -6,13 +6,26 @@ class ActionMatchType(object):
     """ XXX """
     grok.implements(IActionMatchType)
     name = None
+    
+    def apply(self, action, resultMails):
+        """ XXX """
+        pass
 
 class ActionMatchTypeMatch(ActionMatchType, grok.GlobalUtility):
     """ XXX """
     grok.name('match')    
     name = 'if match'
 
+    def apply(self, action, resultMails):
+        """ XXX """
+        if resultMails:
+            action.apply()
+
 class ActionMatchTypeNotMatch(ActionMatchType, grok.GlobalUtility):
     """ XXX """
     grok.name('not match')
     name = 'if does not match'
+    
+    def apply(self, action, resultMails):
+        """ XXX """
+        pass
