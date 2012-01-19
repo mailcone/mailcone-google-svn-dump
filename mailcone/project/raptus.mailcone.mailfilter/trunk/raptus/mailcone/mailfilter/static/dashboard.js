e60651673c419e6cceb6a21242afa3ea6cb05094
samuel.riolo@googlemail.com <samuel.riolo@googlemail.com@2114add4-67ca-9ca6-10be-5e31bf341269>
2012-01-19 22:08:26 +0000

    This is probably the most beautiful svn commit: old stuff is kicked out!

--- mailcone/project/raptus.mailcone.mailfilter/trunk/raptus/mailcone/mailfilter/static/dashboard.js
+++
@@ -1,7 +0,0 @@
-$('.item').live('click', function(){
-	window.location.href = $(this).find('a.itemLink:first').attr('href');
-});
-
-$('.item').live('click', function(e){
-	e.preventDefault();
-});
--- mailcone/project/raptus.mailcone.mfa_core_filter/trunk/raptus/mailcone/mfa_core_filter/contents.py
+++
@@ -1,7 +0,0 @@
-import grok
-
-from raptus.mailcone.mfa_core_filter.interfaces import IFilter
-
-class Filter(object):
-    """XXX"""
-    grok.implements(IFilter)
